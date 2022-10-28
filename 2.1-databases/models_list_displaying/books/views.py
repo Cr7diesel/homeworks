f535270b2d3787d3b224ebe8a228c8885541dd04
Cr7diesel <cr7vin67@mail.ru>
2022-10-28 11:14:57 +0700

    second task done

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
+    book = Book.objects.all()
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': book}
+    return render(request, template, context)
+
+
+def book_page(request, date):
+    template = 'books/books_list.html'
+
+    book_pages = [book['pub_date'] for book in Book.objects.order_by('pub_date').values('pub_date').distinct()]
+
+    paginator = Paginator(book_pages, 1)
+    index = book_pages.index(date)
+    page = paginator.get_page(index + 1)
+
+    books = Book.objects.filter(pub_date=date)
+    context = {
+        'next_date': book_pages[index + 1] if page.has_next() else None,
+        'previous_date': book_pages[index - 1] if page.has_previous() else None,
+        'page': page,
+        'books': books
+    }
+
     return render(request, template, context)
