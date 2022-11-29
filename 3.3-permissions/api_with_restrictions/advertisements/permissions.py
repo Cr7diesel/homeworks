4c8d3d049d50c9e81373975c93baf3741bc99a27
Cr7diesel <cr7vin67@mail.ru>
2022-11-29 14:59:44 +0700

    corrected

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -4,6 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsOwnerOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method == 'POST' or request.user.is_superuser:
-            return True
-        return request.user == obj.creator
+        return request.user == obj.creator or request.user.is_superuser
